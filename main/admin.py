c1daa7ff21d41d6813620ced7526c5fcae4ac119
Pablo Díaz <pablo5_diaz@hotmail.com>
2017-07-18 14:39:12 -0600

    register admin models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from main.models import Product, ReportingPeriod, Profile
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(ReportingPeriod)
+admin.site.register(Profile)
