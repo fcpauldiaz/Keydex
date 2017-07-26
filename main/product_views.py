2cfba65373b3c2fb754de920911e28623664cf3b
Pablo Díaz <pablo5_diaz@hotmail.com>
2017-07-26 13:47:23 -0600

    remove dead code

--- main/product_views.py
+++ main/product_views.py
@@ -27,35 +27,6 @@ def add_product(request):
     form = AsinForm()
     return render(request, 'step_1.html', {'form': form})
 
-# def add_product(request):
-#   if request.method == 'POST':
-#     if request.POST['action'] == "+":
-#       extra = int(float(request.POST['extra'])) + 1
-#       form = AsinForm(initial=request.POST)
-#       formset = formset_factory(FormsetForm, extra=extra)
-#     else:
-#       extra = int(float(request.POST['extra']))
-#       form = AsinForm(request.POST)
-#       formset = formset_factory(FormsetForm, extra=extra)(request.POST)
-
-#       # if form.is_valid() and formset.is_valid():
-#       #   if request.POST['action'] == "Create":
-#       #     for form_c in formset:
-#       #         if not form_c.cleaned_data['delete']:
-#       #             # create data
-#       #   elif request.POST['action'] == "Edit":
-#       #     for form_c in formset:
-#       #         if form_c.cleaned_data['delete']:
-#       #             # delete data
-#       #         else:
-#       #               # create data
-#           # return HttpResponseRedirect('abm_usuarios')
-#   form = AsinForm()
-#   extra = 1
-#   formset = formset_factory(FormsetForm, extra=extra)
-  
-#   return render(request, 'step_1.html', {'form': formset})
-
 @login_required
 def add_keywords(request, asin):
   if request.method == 'GET':
