7774d2395227b25601a6eddf01f0364a58be7151
Pablo Díaz <pablo5_diaz@hotmail.com>
2017-07-27 18:08:02 -0600

    remove console.log

--- keydex/static/js/radio_buttons.js
+++ keydex/static/js/radio_buttons.js
@@ -21,12 +21,10 @@ $('[name="choices_group2"]').on('change', function(e) {
 });
 $('[name="choices_group3"]').on('change', function(e) {
   var value = e.currentTarget.attributes.value.value;
-  console.log(value);
   if (value === 'type7' || value === 'type8' 
     || value === 'type9' || value === 'type10') { 
     var op1 = $('#id_choices_group2_0')[0]
     var op2 = $('#id_choices_group2_1')[0]
-    console.log("op1", op1.checked);
     if (op1.checked === true || op2.checked === false) {
       op2.checked = true;
       op1.checked = false;
