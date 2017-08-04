078eba37ae715f7a1f88f9a4764c97e5cc57c07d
Pablo Díaz <pablo5_diaz@hotmail.com>
2017-08-04 13:09:47 -0600

    add env database

--- keydex/settings.py
+++ keydex/settings.py
@@ -80,16 +80,29 @@ WSGI_APPLICATION = 'keydex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',  
-        'NAME': 'keydex',                      
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',  
+            'NAME': os.environ['DB_NAME'],                      
+            'USER': os.environ['DB_USER'],
+            'PASSWORD': os.environ['DB_PASSOWORD'],
+            'HOST': os.environ['DB_HOST'],
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',  
+            'NAME': 'test',                      
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
+
 
 PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
