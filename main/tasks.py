3e3ae495c918f5bebdf300ee296cbd816b4b3631
Pablo Díaz <pablo5_diaz@hotmail.com>
2018-09-05 14:14:20 -0600

    increase time to 20 s

--- main/tasks.py
+++ main/tasks.py
@@ -9,12 +9,12 @@ from random import randint
 @shared_task
 def index_data(asin, country_host, country_code, keyword, retries):
   returnDictionary = {}  
-  sleep(randint(1, 10))
+  sleep(randint(1, 20))
   page, html = helpers.make_request(asin=asin, host=country_host, keyword=keyword)
   if page == None:
       #log("WARNING: Error in {} found in the extraction. keyword {}".format(product.asin, keyword))
-      sleep(randint(1, 10))
-      if (retries < 2):
+      sleep(randint(1, 20))
+      if (retries < 3):
        return index_data(asin, country_host, country_code, keyword, retries + 1)
       #returnDictionary[keyword] = 'Information not available'
       product_indexing = amazon_product(asin, keyword, country_code)
