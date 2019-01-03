e5af48b5f6fd8e70bd7ea50f4b66c1e700fe06c4
Pablo Díaz <pablo5_diaz@hotmail.com>
2019-01-02 23:30:27 -0600

    test wsgi config

--- keydex/wsgi.py
+++ keydex/wsgi.py
@@ -8,24 +8,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-import time
-import traceback
-import signal
-import sys
+
 from django.core.wsgi import get_wsgi_application
-#from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "keydex.settings")
 
-
-
-try:
-    application = get_wsgi_application()
-    print 'WSGI without exception'
-except Exception:
-    print 'handling WSGI exception'
-    # Error loading applications
-    if 'mod_wsgi' in sys.modules:
-        traceback.print_exc()
-        os.kill(os.getpid(), signal.SIGINT)
-        time.sleep(2.5)
+application = get_wsgi_application()
