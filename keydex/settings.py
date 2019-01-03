403417ee8df577d8aed8ede98aa2dce4040538a4
Pablo Díaz <pablo5_diaz@hotmail.com>
2019-01-02 23:35:41 -0600

    test other config raven

--- keydex/settings.py
+++ keydex/settings.py
@@ -155,7 +155,7 @@ RAVEN_CONFIG = {
     'dsn': env('SENTRY_URL'),
     # If you are using git, you can also automatically configure the
     # release based on the git info.
-    'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+    'release': raven.fetch_git_sha(os.path.abspath(os.getcwd())),
 }
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
